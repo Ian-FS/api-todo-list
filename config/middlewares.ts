648204ed2d0bb9145dd55917a0ca94d68b312927
Ian_França <iiann_2009@hotmail.com>
2024-10-22 16:00:28 -0300

    altera config do middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,6 +7,14 @@ export default [
   'strapi::query',
   'strapi::body',
   'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      rolling: true,
+      renew: true,
+      secure: false,
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
