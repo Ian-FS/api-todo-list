3e8f35606a509e1597b4b49eefb578cbecdb656f
Ian_França <iiann_2009@hotmail.com>
2024-10-23 11:49:24 -0300

    altera middlewares

--- src/middlewares/force-ssl.ts
+++ src/middlewares/force-ssl.ts
@@ -1,14 +1,12 @@
-const { Middleware } = require('koa');
-
-const forceSSL = async (ctx, next) => {
-  if (
-    ctx.headers['x-forwarded-proto'] !== 'https' &&
-    process.env.NODE_ENV === 'production'
-  ) {
-    ctx.redirect(`https://${ctx.host}${ctx.url}`);
-  } else {
-    await next();
-  }
+module.exports = () => {
+  return async (ctx, next) => {
+    if (
+      ctx.headers['x-forwarded-proto'] !== 'https' &&
+      process.env.NODE_ENV === 'production'
+    ) {
+      ctx.redirect(`https://${ctx.host}${ctx.url}`);
+    } else {
+      await next();
+    }
+  };
 };
-
-module.exports = forceSSL;
