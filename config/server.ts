dbbc065081e34cc8f0fe16457e4241aef40301aa
Ian_França <iiann_2009@hotmail.com>
2024-10-28 20:54:04 -0300

    altera arquivo "server.ts"

--- config/server.ts
+++ config/server.ts
@@ -1,9 +1,16 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  proxy: true,
   port: env.int('PORT', 1337),
-  url: env('REACT_APP_BACKEND_URL', 'http://localhost:1337'), // Use a URL de produção corretamente
+  url: env('REACT_APP_BACKEND_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
+    proxy: true, // Confirmado que o proxy está ativado
+  },
+  session: {
+    cookie: {
+      secure: env('NODE_ENV') === 'production', // Força o cookie a ser enviado apenas em HTTPS
+      httpOnly: true, // Recomendado para maior segurança
+      sameSite: 'lax', // Controle de compartilhamento entre sites
+    },
   },
 });
