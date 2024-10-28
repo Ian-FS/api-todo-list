93bad9cc468008c7a62e2af4a15427b30bd7daf0
Ian_França <iiann_2009@hotmail.com>
2024-10-28 20:11:46 -0300

    altera arquivo "server.ts"

--- config/server.ts
+++ config/server.ts
@@ -1,22 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+  proxy: true,
   port: env.int('PORT', 1337),
   url: env('REACT_APP_BACKEND_URL', 'http://localhost:1337'), // Use a URL de produção corretamente
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
-    proxy: true,
-  },
-  session: {
-    cookie: {
-      secure: env.bool('COOKIE_SECURE', env('NODE_ENV') === 'production'), // Assegure que o cookie é seguro apenas em produção
-    },
-  },
-  // Adicione essa seção para forçar HTTPS em produção
-  middleware: {
-    settings: {
-      forceSSL: {
-        enabled: env('NODE_ENV') === 'production',
-      },
-    },
   },
 });
