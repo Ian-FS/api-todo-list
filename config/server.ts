1a08ff26d38c2f0128d7d96920f0ba95d23a6312
Ian_França <iiann_2009@hotmail.com>
2024-10-28 11:14:30 -0300

    altera arquivo "server.ts"

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: true,
   url: env('REACT_APP_BACKEND_URL', 'http://localhost:1337'), // Use a URL de produção corretamente
   app: {
     keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
